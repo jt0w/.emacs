c485b622f4f8a20a0496be93e359d7893c454063
secretval <108518296+SecretVal@users.noreply.github.com>
2025-12-01 21:40:48 +0100

    some more config stuff

--- flake.nix
+++ flake.nix
@@ -10,35 +10,34 @@
     nixpkgs,
     emacs-overlay,
     ...
-    }: let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [emacs-overlay.overlays.default];
-      };
-
-      myEmacs = pkgs.emacsWithPackagesFromUsePackage {
-        package = pkgs.emacs-pgtk;
-        config = ./Config.org;
-        defaultInitFile = true;
-        alwaysEnsure = true;
-        alwaysTangle = true;
-
-
-        extraEmacsPackages = epkgs:  [
-          epkgs.treesit-grammars.with-all-grammars
-          epkgs.use-package
-          epkgs.general
-
-          pkgs.graphviz
-        ];
-      };
-    in {
-      packages.${system}.default = myEmacs;
-
-      apps.${system}.default = {
-        type = "app";
-        program = "${myEmacs}/bin/emacs";
-      };
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [emacs-overlay.overlays.default];
     };
+
+    myEmacs = pkgs.emacsWithPackagesFromUsePackage {
+      package = pkgs.emacs-pgtk;
+      config = ./Config.org;
+      defaultInitFile = true;
+      alwaysEnsure = true;
+      alwaysTangle = true;
+
+      extraEmacsPackages = epkgs: [
+        epkgs.treesit-grammars.with-all-grammars
+        epkgs.use-package
+        epkgs.general
+
+        pkgs.graphviz
+      ];
+    };
+  in {
+    packages.${system}.default = myEmacs;
+
+    apps.${system}.default = {
+      type = "app";
+      program = "${myEmacs}/bin/emacs";
+    };
+  };
 }
